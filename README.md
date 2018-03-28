fddd6c9e7b7aa999e25cf01ee7063609eea06f2e
zzhoujay <zzhoujay@qq.com>
2018-03-28 22:55:44 +0800

    增加了对自定义Html解析器的支持

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RichText
+# RichText [ ![Download](https://api.bintray.com/packages/zzhoujay/maven/richtext/images/download.svg) ](https://bintray.com/zzhoujay/maven/richtext/_latestVersion)
 
 > Android平台下的富文本解析器
 
--- richtext/src/main/java/com/zzhoujay/richtext/parser/Html2SpannedParser.java
+++ richtext/src/main/java/com/zzhoujay/richtext/parser/Html2SpannedParser.java
@@ -2,12 +2,29 @@ package com.zzhoujay.richtext.parser;
 
 import android.text.Html;
 import android.text.Spanned;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by zhou on 16-7-27.
  * Html2SpannedParser
  */
 public class Html2SpannedParser implements SpannedParser {
+
+    private static final String TAG = "Html2SpannedParser";
+
+    private static final String Z_HTML_CLASS_NAME = "com.zzhoujay.html.Html";
+    private static final Method Z_FROM_HTML_METHOD;
+
+    static {
+        Method fromHtml = null;
+        try {
+            fromHtml = Class.forName(Z_HTML_CLASS_NAME).getMethod("fromHtml", String.class, Html.ImageGetter.class, Html.TagHandler.class);
+        } catch (Exception ignore) {
+        }
+        Z_FROM_HTML_METHOD = fromHtml;
+    }
 
     private Html.TagHandler tagHandler;
 
@@ -17,6 +34,13 @@ public class Html2SpannedParser implements SpannedParser {
 
     @Override
     public Spanned parse(String source) {
+        if (Z_FROM_HTML_METHOD != null) {
+            try {
+                return (Spanned) Z_FROM_HTML_METHOD.invoke(null, source);
+            } catch (Exception e) {
+                Log.d(TAG, "Z_FROM_HTML_METHOD invoke failure", e);
+            }
+        }
         return Html.fromHtml(source, null, tagHandler);
     }
 }
